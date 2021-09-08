4d4eaa7f736b179576990ae3d8049d1c73fedd7e
oscarcederberg <os7138ce-s@student.lu.se>
2021-09-08 14:11:46 +0200

    Finish OKParser

--- A1-MiniS/src/java/lang/OKParser.java
+++ A1-MiniS/src/java/lang/OKParser.java
@@ -1,8 +1,69 @@
 package lang;
 
+import lang.ast.LangParser.Terminals;
+
 public class OKParser extends RDPTemplate {
 
     protected void parseProgram(){
-        System.out.println("Hello, world!");
+        parseStmt();
+    }
+
+    protected void parseStmt(){
+        int token = peek();
+        switch (token){
+            case Terminals.FOR:
+                parseForStmt();
+                break;
+            case Terminals.IF:
+                parseIfStmt();
+                break;
+            case Terminals.ID:
+                parseAssignment();
+                break;
+            default:
+                error("Not a stmt");
+        }
+    }
+
+    protected void parseForStmt(){
+        accept(Terminals.FOR);
+        parseAssignment();
+        accept(Terminals.UNTIL);
+        parseExpr();
+        accept(Terminals.DO);
+        parseStmt();
+        accept(Terminals.OD);
+    }
+
+    protected void parseIfStmt(){
+        accept(Terminals.IF);
+        parseExpr();
+        accept(Terminals.THEN);
+        parseStmt();
+        accept(Terminals.FI);
+    }
+
+    protected void parseAssignment(){
+        accept(Terminals.ID);
+        accept(Terminals.ASSIGN);
+        parseExpr();
+    }
+
+    protected void parseExpr(){
+        int token = peek();
+        switch(token){
+            case Terminals.ID:
+                accept(Terminals.ID);
+                break;
+            case Terminals.NUMERAL:
+                accept(Terminals.NUMERAL);
+                break;
+            case Terminals.NOT:
+                accept(Terminals.NOT);
+                parseExpr();
+                break;
+            default:
+                error("Expected an expr, found not an expr");
+        }
     }
 }
